d68ff058a9154deee90c87a76363d586c69de5aa
Fırat Alçın <firatalcin04@gmail.com>
2023-12-24 18:17:40 +0300

    Create Project

---
+++ NTierArchitecture/NTierArchitecture.Entity/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace NTierArchitecture.Entity.Models
+{
+    public sealed class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ NTierArchitecture/NTierArchitecture.Entity/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace NTierArchitecture.Entity.Models
+{
+    public sealed class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Guid CategoryId { get; set; }
+    }
+}
