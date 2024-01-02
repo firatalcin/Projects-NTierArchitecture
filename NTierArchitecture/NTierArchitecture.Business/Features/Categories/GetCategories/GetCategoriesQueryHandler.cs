15a80d27dfd4fefe5ff8d6c8ddb99fb7757cc344
Fırat Alçın <firatalcin04@gmail.com>
2024-01-03 00:09:36 +0300

    MediatR Kütüphanesi ile CQRS Pattern

---
+++ NTierArchitecture/NTierArchitecture.Business/Features/Categories/GetCategories/GetCategoriesQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using NTierArchitecture.Entity.Models;
+using NTierArchitecture.Entity.Repositories;
+
+namespace NTierArchitecture.Business.Features.Categories.GetCategories
+{
+    internal sealed class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, List<Category>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public GetCategoriesQueryHandler(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<List<Category>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+        {
+            return await _categoryRepository.GetAll().OrderBy(x => x.Name).ToListAsync(cancellationToken);
+        }
+    }
+}
---
+++ NTierArchitecture/NTierArchitecture.Business/Features/Categories/UpdateCategory/UpdateCategoryCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using NTierArchitecture.Entity.Models;
+using NTierArchitecture.Entity.Repositories;
+
+namespace NTierArchitecture.Business.Features.Categories.UpdateCategory
+{
+    internal sealed class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpdateCategoryCommandHandler(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+        {
+            _categoryRepository = categoryRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            Category category = await _categoryRepository.GetByIdAsync(p => p.Id == request.id, cancellationToken);
+
+            if (category == null)
+            {
+                throw new ArgumentException("Kategori Bulunamadı");
+            }
+
+            if(category.Name != request.Name)
+            {
+                var isCategoryNameExists = await _categoryRepository.AnyAsync(p => p.Name == request.Name, cancellationToken);
+
+                if (isCategoryNameExists)
+                {
+                    throw new ArgumentException("Bu kategori daha önce oluşturulmuştur");
+                }
+
+                category.Name = request.Name;
+
+                await _unitOfWork.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
